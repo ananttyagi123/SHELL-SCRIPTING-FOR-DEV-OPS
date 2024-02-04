050b1f0a433b45bc1c2a2cf3d8bd451026bd0a20
Anant Tyagi <120208520+ananttyagi123@users.noreply.github.com>
2024-02-04 12:13:12 +0530

    Add files via upload

---
+++ Shell Scripting/Basic.sh
@@ -0,0 +1,29 @@
+# basic Shell commands for Dev- ops 
+
+#                                              Day -1 
+# Basics linux command
+
+# how to make a file -
+
+# to make a file we can use touch command 
+
+$ touch hello.sh
+
+# for making a directory
+
+# mkdir
+
+$ mkdir Hello
+
+# basics command for changing the direcrtory , list of dirctory , printing a statement , remove , rename , concatination of file 
+
+mkdir
+-ls
+-echo "Hello Linux"
+-rm file.sh
+cp
+
+
+
+
+
