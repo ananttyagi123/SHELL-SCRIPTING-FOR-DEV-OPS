4d442cfeb64bfd813bf6493c066ac73a4fa5f8ae
Anant Tyagi <ananttyagi359@gmail.com>
2024-02-04 15:52:19 +0530

    find_&_sudo_user_command

---
+++ Shell Scripting/find_command.sh
@@ -0,0 +1,22 @@
+# find command 
+
+$find 
+
+$ sudo su-
+
+# using sudo you can go anywhere ypu want from the termiinal 
+
+$ su- 
+# the su-  means switch to --
+
+$ sudo -anant
+
+# so using this command we can go to the anant user 
+
+# sudo means root user
+
+
+$ sudo  find /-name.sh
+# you can search to your entire file system  using this file command alog with sudo means root user
+
+ 
