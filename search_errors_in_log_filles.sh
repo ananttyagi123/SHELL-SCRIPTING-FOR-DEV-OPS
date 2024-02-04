3953faa94fe623bea5cef9315f7b63c3cf6cf2b3
Anant Tyagi <ananttyagi359@gmail.com>
2024-02-04 15:40:52 +0530

    reterive error from files

---
+++ search_errors_in_log_filles.sh
@@ -0,0 +1,33 @@
+# script to find out the error in the log files 
+
+# we can simply use pipeline command
+
+$ cat logfiles | grep Error
+# but when we upload our log file to the cloud 
+
+# which is not present in your virtual machine 
+
+$ curl 
+
+# the curl command is ued to retrieve the data from the internet
+# to fetch the API data 
+
+$ curl https://www.google.com |grep Error
+
+# it will give the error from this google server in the log files 
+
+# how to request the API of anything 
+
+$ curl -X GET api.com
+
+# it will give the api data of the api.com or you can say request an API call
+
+$ wget 
+
+# the wget command is used to store the APi data to the log file 
+
+
+
+
+
+
