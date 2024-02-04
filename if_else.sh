1e1a4d51f166c3e360b087dc98d103bcae5100e1
Anant Tyagi <ananttyagi359@gmail.com>
2024-02-04 15:59:35 +0530

    if else statements

---
+++ if_else.sh
@@ -0,0 +1,19 @@
+# if else conditions 
+a=10
+b=20
+# if [expression] 
+# then  
+#     sddsfss
+#     sddsfss
+# else
+#     gfdegkeg
+# fi
+
+if [$a > $b]
+then 
+   echo "a is greater than b"
+else 
+   echo "b is greater than a"
+
+fi
+
